4c7dd7e8def8060ac988370608be66760145f6db
mahendra189 <mahendrank18082004@gmail.com>
2024-12-10 15:40:53 +0530

    check for lower timestamps

--- assignments/timestamp.py
+++ assignments/timestamp.py
@@ -5,6 +5,16 @@ def takeTimestamp():
     sec = int(input("seconds: "))
     return hour,mint,sec
 
+
+ 
+def check(t1,t2):
+    # for hour
+    for i in range(3):
+        if t1[i] > t2[i]:
+            print("Second Timestamp should not be lower than first: ")
+            exit()
+        else:
+            print("Continue finding the diff between the timestamp")
 def printTime(time):
     return f"{time[0]}:{time[1]}:{time[2]}"
 t1 = takeTimestamp()
@@ -12,11 +22,15 @@ printTime(t1)
 t2 = takeTimestamp()
 printTime(t2)
 
+check(t1,t2)
 hour_diff = t2[0]-t1[0]
 min_diff = t2[1]-t1[1]
 sec_diff = t2[2]-t1[2]
 
 print(hour_diff,min_diff,sec_diff)
+
+
+# add conditions to only allow the t1 be earlier than t2
 
 
 # calculate the total time difference
